5d292d361b2b33765c486a3eab277c9763da202e
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-08-07 22:20:40 -0500

    went with modules

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-let createError = require("http-errors");
-let express = require("express");
-let path = require("path");
-let cookieParser = require("cookie-parser");
-
-let indexRouter = require("./routes/index");
-let usersRouter = require("./routes/users");
-
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+// import blurbs from "./scripts/blurbs.js");
+import { art } from "./scripts/blurbs.js";
 let app = express();
 const port = 3000;
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(express.static("./public"));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -33,16 +28,16 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
+  console.log("500");
   res.status(err.status || 500);
   res.render("error");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  console.log("this - ", art);
+  res.render("index.ejs", { art: art });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-module.exports = app;
